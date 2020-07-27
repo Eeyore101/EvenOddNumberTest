21f4382561fc3441b985ecc71cafeaea07f1a723
Łukasz Smak <lukasz.smak@gmail.com>
2020-07-27 21:34:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOrEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w programie testującym parzystość liczby!");
+
+            Console.WriteLine("\nPodaj liczbę, a komputer sprawdzi, czy jest ona parzysta czy nieparzysta: ");
+
+            try
+            {
+                if (!int.TryParse(Console.ReadLine(), out int userNumber))
+                    throw new Exception("Podana wartość jest nieprawidłowa!");
+                if (userNumber % 2 == 0)
+                    Console.WriteLine("Podana liczba jest parzysta.");
+                else
+                    Console.WriteLine("Podana liczba jest nieparzysta.");
+
+                Console.WriteLine("\nDziękuję za skorzystanie z programu!");
+            }
+
+            catch (Exception ex)
+            {
+                //Logowanie do pliku
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("\nNaciśnij dowolny klawisz, aby zakończyć program i wyjść.");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
